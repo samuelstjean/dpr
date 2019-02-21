ea7cae343463f652d54faad17a69fcbd4eeb92c6
Samuel St-Jean <samuel.st-jean@usherbrooke.ca>
2019-02-21 13:31:53 +0100

    Add script

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,12 @@
 # Changelog
 
-## [0.1] - 2019-02-16
+## [v0.1.1] - 2019-02-21
 
-- First release
+## [v0.1] - 2019-02-16
+
+The first release, without a command line interface at the moment.
+
+To install, download the file and run in your favorite terminal
+~~~bash
+pip install dpr-0.1.tar.gz
+~~~
--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='dpr',
-    version='0.1',
+    version='0.1.1',
     author='Samuel St-Jean',
     author_email='samuel@isi.uu.nl',
     packages=find_packages(),
-    # scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
+    scripts=['scripts/dpr'],
     url='https://github.com/samuelstjean/dpr',
     license='LICENSE',
     description='Implementation of "Reducing variability in along-tract analysis with diffusion profile realignment".',
