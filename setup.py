b4d82e7ad2ebb964ced39a07502641c4ae8095b2
Samuel St-Jean <samuel.st-jean@usherbrooke.ca>
2019-12-09 16:18:02 +0100

    edit setup.py for long description

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='dpr',
     version='0.1.2',
@@ -8,9 +11,10 @@ setup(
     packages=find_packages(),
     scripts=['scripts/dpr', 'scripts/dpr_make_fancy_graph'],
     url='https://github.com/samuelstjean/dpr',
-    license='LICENSE',
+    license='MIT',
     description='Implementation of "Reducing variability in along-tract analysis with diffusion profile realignment".',
-    long_description=open('README.md').read(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=['numpy>=1.10',
                       'scipy>=0.19',
                       'matplotlib>=2.0'],
